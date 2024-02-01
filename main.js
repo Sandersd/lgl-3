73eed25f59c983d5467a17fa1318bbadc79ecbb8
Dylan Sanders <dsanders@gala.games>
2024-02-01 01:00:40 -0500

    cdn scrolltrigger

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import './style.css'
 import gsap from 'gsap';
-import ScrollTrigger from 'gsap/dist/scrolltrigger';
 import { neonCursor } from 'https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js'
 
 // NEON BOTTOM SECTION
