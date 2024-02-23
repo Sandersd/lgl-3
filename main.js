fbc5e6fb26681b90e3a0986f83b3df67a9e74bf4
Dylan Sanders <dsanders@gala.games>
2024-02-23 12:09:52 -0500

    testimonials

--- main.js
+++ main.js
@@ -537,7 +537,28 @@ window.addEventListener('load', () => {
 
   const btn1 = document.querySelector('li:nth-child(1) button');
   new HoverButton(btn1);
+
+  animateTestimonials();
 });
+
+// GSAP for Testimonials Scrolling
+function animateTestimonials() {
+  const testimonials = gsap.timeline({
+    defaults: { ease: "linear" },
+    repeat: -1,
+  });
+
+  let totalWidth = document.querySelector('.testimonials').offsetWidth;
+
+  testimonials.to(".testimonials", {
+    x: () => `-${totalWidth}px`,
+    duration: 60, // Adjust duration based on your preference
+    ease: "none",
+    modifiers: {
+      x: gsap.utils.unitize(x => parseFloat(x) % totalWidth)
+    },
+  });
+}
 
 
 const preload = () => {
